933b7da62436dc33cadcb94b8d1cd595d291de67
megankohanarieh <62267170+megankohanarieh@users.noreply.github.com>
2020-04-20 17:56:12 -0700

    Add files via upload
    Cleaned up cluster code, added comment code

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,26 +1,57 @@
 import java.util.ArrayList;
+import java.util.List;
 
-/**
- * This class initiates a cluster which consists of points and a cluster point
- * In this class, we also id'd each cluster
- * 
- * @author Team 5: Peppa Pig
- *
- */
+//The Cluster Class initiates a cluster with points and cluster points
 public class Cluster {
+	
 	public ArrayList<Point> points;
-	public Point clusterpoint;
+	public Point centroid;
 	public int id;
 	
-	/**
-	 * Creates a cluster object with an id
-	 * @param id
-	 */
+	//creates new cluster, uses this. to refer to instance variables
 	public Cluster(int id) {
 		this.id = id;
-		this.points = new ArrayList<Point>();
-		this.clusterpoint = null;
+		this.points = new ArrayList();
+		this.centroid = null;
 	}
 	
+	//creates a getter for points
+	public List getPoints() {
+		return points;
+	}
+	//method that adds point to ArrayList points
+	public void addPoint(Point point) {
+		points.add(point);
+	}
+	//creates setter for points
+	public void setPoints(ArrayList<Point> points) {
+		this.points = points;
+	}
+	//creates getter for Centroid
+	public Point getCentroid() {
+		return centroid;
+	}
+	//creates setter for centroid
+	public void setCentroid(Point centroid) {
+		this.centroid = centroid;
+	}
+	//creates getter for id
+	public int getId() {
+		return id;
+	}
 	
+	public void clear() {
+		points.clear();
+	}
+	//creates system of print lines in order to plot the cluster
+	public void plotCluster() {
+		System.out.println("[Cluster: " +id+"]");
+		System.out.println("[Centroid: "+centroid+"]");
+		System.out.println("[Points \n");
+		for(Point p : points) {
+			System.out.println(p);
+		}
+		System.out.println("]");
+	}
+
 }
